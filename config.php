9221f32951b5d71ead20f059c9a5285669aca092
Mohamed Zaher Saadeldin <mzaher@redhat.com>
2026-01-15 22:56:30 +0400

    Update config.php with DB_NAME variable

--- config.php
+++ config.php
@@ -5,10 +5,10 @@
  *
  */
 
-$host       = getenv('HOST');
+$host       = getenv('DB_HOST');
 $username   = getenv('DB_USER');
 $password   = getenv('DB_PASSWORD');
-$dbname     = "test";
+$dbname     = getenv('DB_NAME');
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
